772fc0ce037bf48caa97f91ed1d7044963f339b3
nadiatkachuk <126887250+nadiatkachuk@users.noreply.github.com>
2023-10-07 20:08:58 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import WeatherSearch from "./WeatherSearch";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search Engine</h1>
+      <WeatherSearch />
+    </div>
+  );
+}
+
+export default App;
