3bbd11ff3eec9c2357a524b87b972c305dcaa0e0
nadiatkachuk <126887250+nadiatkachuk@users.noreply.github.com>
2023-10-07 21:18:23 +0300

    added style to footer

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import WeatherSearch from "./WeatherSearch";
 function App() {
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
-      <WeatherSearch />
-      <br />
-      <footer>
-        <a href="https://github.com/nadiatkachuk/weather-app-react-project">
-          Coded by Nadiia Tkachuk
-        </a>
-      </footer>
+      <body>
+        <h1>Weather Search Engine</h1>
+        <WeatherSearch />
+        <br />
+        <footer>
+          <a href="https://github.com/nadiatkachuk/weather-app-react-project">
+            Coded by Nadiia Tkachuk
+          </a>
+        </footer>
+      </body>
     </div>
   );
 }
